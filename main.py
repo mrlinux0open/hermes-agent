c5cdf686ab9aadb7f211dcabefebfae0c6d24352
abbas <133507749+mrlinux0open@users.noreply.github.com>
2026-07-15 01:16:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-import subprocess
-import sys
-
-subprocess.run([
-    sys.executable,
-    "-m",
-    "pip",
-    "install",
-    "git+https://github.com/NousResearch/hermes-agent.git"
-], check=True)
-
-subprocess.run(["hermes setup"])
+a = 2
+print(a)
